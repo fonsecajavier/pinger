b56c0a3c674c9e62dd5a79c57cec1f3cf7fb8577
Javier Fonseca <fonsecajavier@gmail.com>
2020-07-17 22:37:07 -0500

    Refactor into a class service

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Instructions
 
 1. `bundle install`
-2. Open `pinger.rb` and edit CHOST constant to contain the IP/address of the server to be checked.
-3. To run just do `ruby pinger.rb` from your terminal. Any ping errors will be shown in the screen and appended to `log.txt` file.
+2. Open `pinger.rb` and edit the last line that includes the host IP/address that is being passed as a passed parameter to the `Pinger` class.
+3. To run just do `ruby pinger.rb` from your terminal. Any ping errors will be shown in the screen and appended to `log.txt` file. **Protip:** just run `tail -f log.txt` in another terminal.
 
 ### Note:
 Make sure the machine in which you run this script doesn't go to sleep. In MacOS you might want to download something like Amphetamine from the Mac App Store.
--- pinger.rb
+++ pinger.rb
@@ -1,36 +1,54 @@
 require "net/ping"
+require "fileutils"
 
-CHOST = "192.168.5.1"
-MAX_PING_FAIL_TIME = 5
-LOG_FILE = "log.txt"
+class Pinger
+  def initialize(host, max_ping_fail_time = 5, log_file_name = "log.txt")
+    @host = host
+    @max_ping_fail_time = max_ping_fail_time
+    @log_file_name = log_file_name
+  end
 
-def up?(host)
-  check = Net::Ping::External.new(host, nil, 1)
-  check.ping?
-end
-
-fail_time = Time.now
-
-puts "Welcome terran. Please ensure this machine won't go to sleep. To stop, hit CTRL+C"
-
-while true
-  if up?(CHOST)
-    puts "PING OK"
+  def call
     fail_time = nil
-  else
-    puts "PING LOST"
-    if fail_time.nil?
-      fail_time = Time.now
-    else
+    FileUtils.touch(@log_file_name)
+
+    puts "Welcome terran. We're about to ping #{@host}."
+    puts "Please ensure this machine won't go to sleep. To stop, hit CTRL+C"
+
+    while true
+      sleep 1
+
+      if host_is_up?
+        fail_time = nil
+        puts "PING OK"
+        next
+      end
+
+      puts "PING LOST"
+
+      if fail_time.nil?
+        fail_time = Time.now
+        next
+      end
+
       now = Time.now
       elapsed_time = now - fail_time
-      if elapsed_time >= MAX_PING_FAIL_TIME
-        err_msg = "Couldn't get response from #{CHOST} at #{now} after #{elapsed_time.to_i} seconds"
+
+      if elapsed_time >= @max_ping_fail_time
+        err_msg = "Couldn't get response from #{@host} since #{fail_time} after #{elapsed_time.to_i} seconds"
         puts err_msg
-        File.open(LOG_FILE, "a") { |f| f.write(err_msg + "\n") }
+        File.open(@log_file_name, "a") { |f| f.write(err_msg + "\n") }
         fail_time = nil
       end
     end
   end
-  sleep 1
+
+  private
+
+  def host_is_up?
+    check = Net::Ping::External.new(@host, nil, 1)
+    check.ping?
+  end
 end
+
+Pinger.new("192.168.5.1").call
